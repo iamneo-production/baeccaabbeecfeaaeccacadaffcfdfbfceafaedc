2087a513fdcd6f24e894d647b5d558bfff6c1516
Iamneo <techadmin@iamneo.ai>
2023-08-04 14:00:56 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/repository/SpringappRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/SpringappRepository.java
@@ -1,3 +1,8 @@
-public class SpringappRepository {
+package com.examly.springapp.repository;
+import org.springframework.data.jpa.repository.JpaRepository;  
+import org.springframework.stereotype.Repository;
+import com.examly.springapp.model.Springapp;  
+@Repository
+public interface SpringappRepository extends JpaRepository<Springapp,Integer> {
     
 }
